db717630e882194e3b01d3aaf1eb7d37407e1700
JeongMin <104041902+kaladi1@users.noreply.github.com>
2024-10-21 17:21:45 +0900

    [Bronze V] Title: A-B, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int A,B,C;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        C = A-B;
+        System.out.println(C);
+    }
+}
