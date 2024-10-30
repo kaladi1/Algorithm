8fff1581cc863e2972bd8004f3bd79267639f621
JeongMin <104041902+kaladi1@users.noreply.github.com>
2024-10-30 14:08:53 +0900

    [Bronze V] Title: A/B, Time: 188 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        double A,B,C;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+        C = A/B;
+        System.out.println(C);
+    }
+}
