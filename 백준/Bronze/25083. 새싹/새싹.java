2f0bc9084ca55556c54129ad8dad950aae4366d9
JeongMin <104041902+kaladi1@users.noreply.github.com>
2025-03-03 13:32:15 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
