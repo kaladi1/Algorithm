b4f9ae6f7ec22f19a7998dc4c076e4dc676a035b
JeongMin <104041902+kaladi1@users.noreply.github.com>
2025-05-10 20:46:46 +0900

    [Silver IV] Title: 대칭 차집합, Time: 700 ms, Memory: 83484 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        Set<Integer> set = new HashSet<>();
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=0 ; i < A ; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=0 ; i < B ; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if(set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
