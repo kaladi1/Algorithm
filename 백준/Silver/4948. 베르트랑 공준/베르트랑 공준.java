2e0d1d1c0eb524b185f8f663856a06b7dee10111
JeongMin <104041902+kaladi1@users.noreply.github.com>
2025-05-16 22:45:46 +0900

    [Silver II] Title: 베르트랑 공준, Time: 704 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+
+            int count = 0;
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (isPrime(i)) {
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
