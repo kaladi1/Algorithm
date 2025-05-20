32c11655f402da9c9866ed9fdf3166b1c4f035ce
JeongMin <104041902+kaladi1@users.noreply.github.com>
2025-05-21 00:15:13 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 284 ms, Memory: 20120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String s = br.readLine();
+            if (s.equals(".")) {
+                break;
+            }
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(' || c == '[') {
+                    stack.push(c);
+                } else if (c == ')') {
+                    if (!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                } else if (c == ']') {
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                }
+            }
+            if (stack.isEmpty()) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
