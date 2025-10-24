cb74316320e6fa4d71302917287a89dcab02b514
JeongMin <104041902+kaladi1@users.noreply.github.com>
2025-10-24 21:19:17 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
@@ -0,0 +1,9 @@
+dir = ['N', 'E', 'S', 'W']
+now = 0
+for _ in range(10):
+    n = int(input())
+    if n == 1: now = (now + 1) % 4
+    elif n == 2: now = (now + 2) % 4
+    else: now = (now + 3) % 4
+
+print(dir[now])
