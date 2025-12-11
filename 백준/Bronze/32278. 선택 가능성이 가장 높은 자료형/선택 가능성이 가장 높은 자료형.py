28aa59b30c434aea9bdf45cb5d5c5840bab3057f
JeongMin <104041902+kaladi1@users.noreply.github.com>
2025-12-11 19:02:14 +0900

    [Bronze IV] Title: 선택 가능성이 가장 높은 자료형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+if N >= -32768 and N <= 32767 : 
+    print('short')
+elif N >= -2147483648 and N <= 2147483647 : 
+    print('int')
+else : 
+    print('long long')
