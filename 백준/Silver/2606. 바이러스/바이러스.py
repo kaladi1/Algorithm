d9c2a5ab3549616eafe4532124175086488f9adc
JeongMin <104041902+kaladi1@users.noreply.github.com>
2026-03-07 10:03:52 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,36 @@
+cnt=0
+def DFS(virus):
+    global cnt
+    visited[virus]=1
+
+    for i in network[virus]:
+        if (visited[i]==0):
+            DFS(i)
+            cnt+=1
+
+def BFS(virus):
+    global cnt
+    visited[virus] = 1
+    queue = [virus]
+    while queue:
+        for i in network[queue.pop()]:
+            if (visited[i]==0):
+                visited[i]=1
+                queue.insert(0, i)
+                cnt+=1
+
+                
+N= int(input())
+link = int(input())
+
+network = [[]*(N+1) for _ in range(N+1)]
+
+for i in range(link):
+    a, b = map(int, input().split())
+    network[a].append(b)
+    network[b].append(a)
+
+visited = [0]*(N+1)
+BFS(1)
+
+print(cnt)
